575cf5bc2b960fec0edf6c4b5020f02becbda9ec
OdenTakashi <odentakashidesu@icloud.com>
2023-08-04 11:04:47 +0900

    テスト名を修正

--- spec/requests/welcome_spec.rb
+++ spec/requests/welcome_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Welcomes', type: :request do
-  describe 'GET /index' do
+  describe 'GET /welcome' do
     it 'returns http success' do
       get '/welcome'
       expect(response).to have_http_status(:success)
