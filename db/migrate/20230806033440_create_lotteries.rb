dd1d1db3be362371fdd3e396ddea4b2c8929cc23
OdenTakashi <odentakashidesu@icloud.com>
2023-08-06 15:13:01 +0900

    migrationファイルを作成

--- db/migrate/20230806033440_create_lotteries.rb
+++ db/migrate/20230806033440_create_lotteries.rb
@@ -1,9 +1,11 @@
 class CreateLotteries < ActiveRecord::Migration[7.0]
   def change
-    create_table :lotteries do |t|
-      t.boolean :name_field_enabled
-      t.boolean :note_field_enabled
-      t.date :deadline
+    enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto')
+    create_table :lotteries, id: :uuid do |t|
+      t.references :user, null: false, foreign_key: true, index: true
+      t.boolean :name_field_enabled, null: false, default: false
+      t.boolean :note_field_enabled, null: false, default: false
+      t.date :deadline, null: false
 
       t.timestamps
     end
