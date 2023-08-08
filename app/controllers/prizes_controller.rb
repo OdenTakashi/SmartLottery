b30793c46dc2365c77a3d31052c1bd131b145683
OdenTakashi <odentakashidesu@icloud.com>
2023-08-08 17:31:59 +0900

    bin/lintを実行

--- app/controllers/prizes_controller.rb
+++ app/controllers/prizes_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class PrizesController < ApplicationController
-  before_action :set_prize, only: %i[ show edit update destroy ]
+  before_action :set_prize, only: %i[show edit update destroy]
 
   # GET /prizes or /prizes.json
   def index
@@ -7,8 +9,7 @@ class PrizesController < ApplicationController
   end
 
   # GET /prizes/1 or /prizes/1.json
-  def show
-  end
+  def show; end
 
   # GET /prizes/new
   def new
@@ -16,8 +17,7 @@ class PrizesController < ApplicationController
   end
 
   # GET /prizes/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /prizes or /prizes.json
   def create
@@ -25,7 +25,7 @@ class PrizesController < ApplicationController
 
     respond_to do |format|
       if @prize.save
-        format.html { redirect_to prize_url(@prize), notice: "Prize was successfully created." }
+        format.html { redirect_to prize_url(@prize), notice: 'Prize was successfully created.' }
         format.json { render :show, status: :created, location: @prize }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class PrizesController < ApplicationController
   def update
     respond_to do |format|
       if @prize.update(prize_params)
-        format.html { redirect_to prize_url(@prize), notice: "Prize was successfully updated." }
+        format.html { redirect_to prize_url(@prize), notice: 'Prize was successfully updated.' }
         format.json { render :show, status: :ok, location: @prize }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,19 +52,20 @@ class PrizesController < ApplicationController
     @prize.destroy
 
     respond_to do |format|
-      format.html { redirect_to prizes_url, notice: "Prize was successfully destroyed." }
+      format.html { redirect_to prizes_url, notice: 'Prize was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_prize
-      @prize = Prize.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def prize_params
-      params.require(:prize).permit(:name, :winners_count, :winning_email_subject, :winning_email_body)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_prize
+    @prize = Prize.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def prize_params
+    params.require(:prize).permit(:name, :winners_count, :winning_email_subject, :winning_email_body)
+  end
 end
--- spec/factories/prizes.rb
+++ spec/factories/prizes.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :prize do
-    name { "MyString" }
+    name { 'MyString' }
     winners_count { 1 }
-    winning_email_subject { "MyString" }
-    winning_email_body { "MyString" }
+    winning_email_subject { 'MyString' }
+    winning_email_body { 'MyString' }
   end
 end
