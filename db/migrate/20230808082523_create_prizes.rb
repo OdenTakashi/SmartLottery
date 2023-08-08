06c3cac4068d48d5c741ebe68d526e3b0aca2e61
OdenTakashi <odentakashidesu@icloud.com>
2023-08-08 17:41:08 +0900

    migrateファイルを作成

--- db/migrate/20230808082523_create_prizes.rb
+++ db/migrate/20230808082523_create_prizes.rb
@@ -1,10 +1,11 @@
 class CreatePrizes < ActiveRecord::Migration[7.0]
   def change
     create_table :prizes do |t|
-      t.string :name
-      t.integer :winners_count
-      t.string :winning_email_subject
-      t.string :winning_email_body
+      t.references :lottery, null: false, foreign_key: true, index: true
+      t.string :name, null: false
+      t.integer :winners_count, null: false
+      t.string :winning_email_subject, null: false
+      t.string :winning_email_body, null: false
 
       t.timestamps
     end
