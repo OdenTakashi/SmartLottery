956ae513e1bae98f2f03dabf49f781057b67d9f2
OdenTakashi <odentakashidesu@icloud.com>
2023-08-08 10:39:05 +0900

    lotteries_pathをroot_pathに変更

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -25,6 +25,6 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
 
   def after_confirmation_path_for(_resource_name, resource)
     sign_in(resource)
-    lotteries_path
+    root_path
   end
 end
