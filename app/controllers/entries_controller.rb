a12c31be35de3c3e4cd0aa0d5a70afb0a94fcbf4
OdenTakashi <odentakashidesu@icloud.com>
2023-08-11 11:39:29 +0900

    bin/lint

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class EntriesController < ApplicationController
-  before_action :set_entry, only: %i[ show edit update destroy ]
-  before_action :set_lottery, only: %i[ index new create show edit update destroy ]
+  before_action :set_entry, only: %i[show edit update destroy]
+  before_action :set_lottery, only: %i[index new create show edit update destroy]
 
   # GET /entries or /entries.jsonå
   def index
@@ -8,8 +10,7 @@ class EntriesController < ApplicationController
   end
 
   # GET /entries/1 or /entries/1.json
-  def show
-  end
+  def show; end
 
   # GET /entries/new
   def new
@@ -17,8 +18,7 @@ class EntriesController < ApplicationController
   end
 
   # GET /entries/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /entries or /entries.json
   def create
@@ -27,7 +27,7 @@ class EntriesController < ApplicationController
 
     respond_to do |format|
       if @entry.save
-        format.html { redirect_to lottery_entry_path(@lottery, @entry), notice: "Entry was successfully created." }
+        format.html { redirect_to lottery_entry_path(@lottery, @entry), notice: 'Entry was successfully created.' }
         format.json { render :show, status: :created, location: @entry }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -40,7 +40,7 @@ class EntriesController < ApplicationController
   def update
     respond_to do |format|
       if @entry.update(entry_params)
-        format.html { redirect_to lottery_entry_path(@lottery, @entry), notice: "Entry was successfully updated." }
+        format.html { redirect_to lottery_entry_path(@lottery, @entry), notice: 'Entry was successfully updated.' }
         format.json { render :show, status: :ok, location: @entry }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -54,23 +54,24 @@ class EntriesController < ApplicationController
     @entry.destroy
 
     respond_to do |format|
-      format.html { redirect_to lottery_entries_path(@lottery), notice: "Entry was successfully destroyed." }
+      format.html { redirect_to lottery_entries_path(@lottery), notice: 'Entry was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_entry
-      @entry = Entry.find(params[:id])
-    end
 
-    def set_lottery
-      @lottery = Lottery.find(params[:lottery_id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_entry
+    @entry = Entry.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def entry_params
-      params.require(:entry).permit(:email, :name, :note)
-    end
+  def set_lottery
+    @lottery = Lottery.find(params[:lottery_id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def entry_params
+    params.require(:entry).permit(:email, :name, :note)
+  end
 end
--- spec/factories/entries.rb
+++ spec/factories/entries.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :entry do
-    email { "MyString" }
-    name { "MyString" }
-    note { "MyString" }
+    email { 'MyString' }
+    name { 'MyString' }
+    note { 'MyString' }
   end
 end
