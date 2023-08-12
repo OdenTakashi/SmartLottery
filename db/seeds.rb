0ac85a9ee2016f032c8859e924c83943846341f2
OdenTakashi <odentakashidesu@icloud.com>
2023-08-12 14:07:39 +0900

    サンプルデータの追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,32 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+User.create!(email: 'user1@example.com', password: 'testtest', confirmed_at: Time.current)
+
+lottery1 = Lottery.create!(
+  {
+    user_id: 1,
+    name_field_enabled: false,
+    note_field_enabled: false,
+    deadline: '2023-9-1',
+    prizes_attributes: [
+      {
+        name: 'サンプル賞品1',
+        winners_count: 2,
+        winning_email_subject: 'おめでとうございます',
+        winning_email_body: '当選おめでとうございます'
+      },
+      {
+        name: 'サンプル賞品2',
+        winners_count: 3,
+        winning_email_subject: 'ナイス',
+        winning_email_body: 'おめでとうおめでとうです'
+      }
+    ]
+  }
+)
+
+(1..6).each do |n|
+  lottery1.entries.create!(
+    email: "entry#{n}@example.com"
+  )
+end
