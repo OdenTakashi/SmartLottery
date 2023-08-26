2c8656f6a5f3d826cf5638e63f3042d8b75d37e9
OdenTakashi <odentakashidesu@icloud.com>
2023-08-26 20:34:06 +0900

    emailのformatのvalidationを削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
