378a40ff861058d09e6da172b05abbb65d6ba9fd
OdenTakashi <odentakashidesu@icloud.com>
2023-09-19 19:27:14 +0900

    応募フォームに名前が必要とされている時に、入力しないとエラーになるかのテストを追加

--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -3,5 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe Entry, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    let(:user) { create(:user) }
+    let(:lottery) { create(:lottery, user:) }
+
+    it 'is invalid without name' do
+      invalid_entry = build(:entry, lottery:, name: nil)
+      invalid_entry.valid?
+      expect(invalid_entry.errors[:name]).to include('を入力してください')
+    end
+  end
 end
