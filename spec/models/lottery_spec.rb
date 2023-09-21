aba2e27cb8bc50b9a29531172a90666e7d2bb5a8
OdenTakashi <odentakashidesu@icloud.com>
2023-09-21 16:50:59 +0900

    runメソッドのテストを追加

--- spec/models/lottery_spec.rb
+++ spec/models/lottery_spec.rb
@@ -3,5 +3,21 @@
 require 'rails_helper'
 
 RSpec.describe Lottery, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  include LotteriesHelper
+
+  describe 'run' do
+    let!(:closed_lottery) { create(:lottery, deadline: Time.zone.yesterday) }
+    let!(:open_lottery) { create(:lottery, deadline: Time.zone.today) }    
+
+    it 'lottery executed' do
+      Lottery.run
+      expect(lottery_executed?(closed_lottery)).to eq true
+    end
+
+    it 'not lottery executed' do
+      Lottery.run
+      expect(lottery_executed?(open_lottery)).to eq false
+    end
+
+  end
 end
