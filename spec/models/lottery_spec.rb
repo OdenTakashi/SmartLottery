8ecaf0694178985bc434af47b4d35907bac6f6b2
OdenTakashi <odentakashidesu@icloud.com>
2023-09-21 17:00:55 +0900

    前日締め切りの抽選会のみ抽選されるかをテスト

--- spec/models/lottery_spec.rb
+++ spec/models/lottery_spec.rb
@@ -8,17 +8,22 @@ RSpec.describe Lottery, type: :model do
   describe 'run' do
     let!(:closed_lottery) { create(:lottery, deadline: Time.zone.yesterday) }
     let!(:open_lottery) { create(:lottery, deadline: Time.zone.today) }
+    let!(:closed_lottery_2days_ago) { create(:lottery, deadline: Time.zone.today.ago(2.days)) }
 
     before do
       described_class.run
     end
 
     it 'lottery executed' do
-      expect(lottery_executed?(closed_lottery)).to eq true
+      expect(lottery_executed?(closed_lottery)).to be_truthy
     end
 
-    it 'not lottery executed' do
-      expect(lottery_executed?(open_lottery)).to eq false
+    it 'open lottery do not executed' do
+      expect(lottery_executed?(open_lottery)).to be_falsey
+    end
+
+    it 'closed lottery 2days ago do not executed' do
+      expect(lottery_executed?(closed_lottery_2days_ago)).to be_falsey
     end
   end
 end
