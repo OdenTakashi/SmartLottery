6265e8079498b3e7fbe330e15da1547b37eb4293
OdenTakashi <odentakashidesu@icloud.com>
2023-09-21 16:56:24 +0900

    冗長な箇所を修正

--- spec/models/lottery_spec.rb
+++ spec/models/lottery_spec.rb
@@ -9,13 +9,15 @@ RSpec.describe Lottery, type: :model do
     let!(:closed_lottery) { create(:lottery, deadline: Time.zone.yesterday) }
     let!(:open_lottery) { create(:lottery, deadline: Time.zone.today) }
 
-    it 'lottery executed' do
+    before do
       described_class.run
+    end
+
+    it 'lottery executed' do
       expect(lottery_executed?(closed_lottery)).to eq true
     end
 
     it 'not lottery executed' do
-      described_class.run
       expect(lottery_executed?(open_lottery)).to eq false
     end
   end
