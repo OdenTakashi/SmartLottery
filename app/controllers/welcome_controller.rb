ffe1d286dcae24bab8edc1cdc3b668f3bcd538f7
OdenTakashi <odentakashidesu@icloud.com>
2023-09-22 10:52:10 +0900

    プライバシーポリシーと利用規約のリンクを追加

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,4 +4,8 @@ class WelcomeController < ApplicationController
   def index
     redirect_to root_path if user_signed_in?
   end
+
+  def terms; end
+
+  def privacy_policy; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'lotteries#index'
   get 'welcome', to: 'welcome#index'
+  get 'privacy_policy', to: 'welcome#privacy_policy', as: :privacy_policy
+  get 'terms', to: 'welcome#terms', as: :terms
   resources :lotteries do
     resources :entries, only: [:new, :create]
   end
