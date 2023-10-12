7c78d669b9445ec7665445df8dbcc0802f74e165
OdenTakashi <odentakashidesu@icloud.com>
2023-10-12 10:39:12 +0900

    Entry#update_prizeのテストを追加

--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -12,4 +12,16 @@ RSpec.describe Entry, type: :model do
       expect(invalid_entry.errors[:name]).to include('を入力してください')
     end
   end
+
+  describe '#update_prize' do
+    let(:lottery) { create(:lottery) }
+    let(:entry) { create(:entry, lottery:) }
+    let(:prize) { create(:prize, lottery:) }
+
+    it 'update successful' do
+      expect(entry.prize).to be_nil
+      entry.update_prize(prize)
+      expect(entry.prize).not_to be_nil
+    end
+  end
 end
