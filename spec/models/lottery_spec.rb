5408635dadd36c8ffa29a3152570638fa56f12cb
OdenTakashi <odentakashidesu@icloud.com>
2023-10-14 10:38:15 +0900

    scopeのテストを追加

--- spec/models/lottery_spec.rb
+++ spec/models/lottery_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 RSpec.describe Lottery, type: :model do
   include LotteriesHelper
 
-  describe 'run' do
+  describe 'execute' do
     let!(:closed_lottery) { create(:lottery, :skip_validate, deadline: Time.zone.yesterday) }
     let!(:open_lottery) { create(:lottery, deadline: Time.zone.today) }
     let!(:closed_lottery_2days_ago) { create(:lottery, :skip_validate, deadline: Time.zone.today.ago(2.days)) }
@@ -24,6 +24,23 @@ RSpec.describe Lottery, type: :model do
 
     it 'closed lottery 2days ago do not executed' do
       expect(lottery_executed?(closed_lottery_2days_ago)).to be_falsey
+    end
+  end
+
+  describe 'scope' do
+    describe 'closed_lottery' do
+      subject { described_class.closed_lottery }
+
+      let!(:lottery_closed_yesterday) { create(:lottery, :skip_validate, deadline: Time.zone.yesterday) }
+      let!(:lottery_closed_today) { create(:lottery, :skip_validate, deadline: Time.zone.today) }
+
+      it 'include lottery_closed_yesterday' do
+        expect(described_class.closed_lottery).to include lottery_closed_yesterday
+      end
+
+      it 'do not include lottery_closed_today' do
+        expect(described_class.closed_lottery).not_to include lottery_closed_today
+      end
     end
   end
 end
