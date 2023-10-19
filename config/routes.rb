7b450f1c61d0935c33c94828046904595c7b59f8
OdenTakashi <odentakashidesu@icloud.com>
2023-10-19 10:55:00 +0900

    welcomeのルーティングを絞った

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'privacy_policy', to: 'welcome#privacy_policy', as: :privacy_policy
   get 'terms', to: 'welcome#terms', as: :terms
 
-  resources :welcome
+  resources :welcome, only: (:index)
 
   resources :lotteries do
     resources :entries, only: [:new, :create]
