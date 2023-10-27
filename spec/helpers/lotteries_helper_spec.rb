244cd43d387a366e1dd105ec6b85deb4b1c350e8
OdenTakashi <odentakashidesu@icloud.com>
2023-10-27 12:03:38 +0900

    [リファクタリング]spec/helpers/lotteries_helper_spec.rb

--- spec/helpers/lotteries_helper_spec.rb
+++ spec/helpers/lotteries_helper_spec.rb
@@ -4,29 +4,40 @@ require 'rails_helper'
 
 RSpec.describe LotteriesHelper, type: :helper do
   let(:lottery) { create(:lottery) }
-  let!(:lottery_executed) { create(:lottery, :skip_validate, deadline: Time.zone.yesterday) }
+  let!(:lottery_closing_yesterday) { create(:lottery, :skip_validate, deadline: Time.zone.yesterday) }
 
-  it 'lottery_executed?' do
-    Lottery.execute
-    expect(lottery_executed?(lottery_executed)).to eq true
-    expect(lottery_executed?(lottery)).to eq false
+  describe '#lottery_executed?' do
+    before do
+      Lottery.execute
+    end
+
+    it 'returns true for lottery closed yesterday' do
+      expect(lottery_executed?(lottery_closing_yesterday)).to eq true
+    end
+
+    it 'returns false for lottery closing tomorrow' do
+      expect(lottery_executed?(lottery)).to eq false
+    end
   end
 
-  it 'closed?' do
-    expect(closed?(lottery_executed)).to eq true
-    expect(closed?(lottery)).to eq false
+  describe '#closed?' do
+    it 'returns true for lottery closed yesterday' do
+      expect(closed?(lottery_closing_yesterday)).to eq true
+    end
+
+    it 'returns false for lottery closing tomorrow' do
+      expect(closed?(lottery)).to eq false
+    end
   end
 
-  it 'total_winners' do
-    expect(total_winners(lottery)).to eq 6
-  end
-
-  it 'extra_prizes_count' do
-    expect(extra_prizes_count(lottery)).to eq 1
+  describe '#total_winners' do
+    it 'returns 6 for total winners' do
+      expect(total_winners(lottery)).to eq 6
+    end
   end
 
   describe '#description_template' do
-    it 'display_template_word' do
+    it 'displays template' do
       expect(description_template).to eq "この抽選会はXXX株式会社の提供で行われます。\n備考にはxxxを書いてください\n"
     end
   end
