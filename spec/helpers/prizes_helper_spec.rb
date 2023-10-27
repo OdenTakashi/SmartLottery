a43d55391e15e5a56f4c1032b1456bec4c6850c8
OdenTakashi <odentakashidesu@icloud.com>
2023-10-27 12:14:04 +0900

    [リファクタリング]spec/helpers/prizes_helper_spec.rb

--- spec/helpers/prizes_helper_spec.rb
+++ spec/helpers/prizes_helper_spec.rb
@@ -3,7 +3,9 @@
 require 'rails_helper'
 
 RSpec.describe PrizesHelper, type: :helper do
-  it 'winning_message_template' do
-    expect(winning_message_template).to eq "当選おめでとうございます。\n\n発送などについての情報を以下のメールアドレスまで送信ください。\n\nowner@example.com\n\n"
+  describe '#winning_message_template' do
+    it 'displays a winning message template' do
+      expect(winning_message_template).to eq "当選おめでとうございます。\n\n発送などについての情報を以下のメールアドレスまで送信ください。\n\nowner@example.com\n\n"
+    end
   end
 end
