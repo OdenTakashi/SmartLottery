a68b1586418b06aed0d366fe3d8d39f25cde4c27
naoki.kodama <odentakashidesu2@gmail.com>
2024-08-18 22:20:57 +0900

    共通化できる部分を修正

--- spec/requests/lottery_spec.rb
+++ spec/requests/lottery_spec.rb
@@ -3,13 +3,16 @@
 require 'rails_helper'
 
 RSpec.describe Lottery, type: :request do
+  let(:user) { create(:user) }
+
+  before do
+    sign_in user
+  end
+
   describe 'GET /lotteries' do
     subject(:request_index) { get lotteries_path }
 
-    let(:user) { create(:user) }
-
     it 'return 200 status' do
-      sign_in user
       request_index
 
       expect(response).to have_http_status(:ok)
@@ -19,11 +22,9 @@ RSpec.describe Lottery, type: :request do
   describe 'GET /lotteries/:id/edit' do
     subject(:request_edit) { get edit_lottery_path(lottery) }
 
-    let(:user) { create(:user) }
     let(:lottery) { create(:lottery) }
 
     it 'return 200 status' do
-      sign_in user
       request_edit
 
       expect(response).to have_http_status(:ok)
@@ -33,11 +34,9 @@ RSpec.describe Lottery, type: :request do
   describe 'GET /lotteries/:id' do
     subject(:request_show) { get lottery_path(lottery) }
 
-    let(:user) { create(:user) }
     let(:lottery) { create(:lottery) }
 
     it 'return 200 status' do
-      sign_in user
       request_show
 
       expect(response).to have_http_status(:ok)
@@ -47,10 +46,7 @@ RSpec.describe Lottery, type: :request do
   describe 'GET /lotteries/new' do
     subject(:request_new) { get new_lottery_path }
 
-    let(:user) { create(:user) }
-
     it 'return 200 status' do
-      sign_in user
       request_new
 
       expect(response).to have_http_status(:ok)
@@ -58,9 +54,8 @@ RSpec.describe Lottery, type: :request do
   end
 
   describe 'POST /lotteries' do
-    subject(:request_create) { post lotteries_path(params) }
+    subject(:request_create) { post lotteries_path, params: }
 
-    let(:user) { create(:user) }
     let(:params) do
       { lottery: {
         user_id: user.id,
@@ -80,8 +75,6 @@ RSpec.describe Lottery, type: :request do
     end
 
     it 'create lottery successful' do
-      sign_in user
-
       expect { request_create }.to change(described_class, :count).by(1)
       expect(response).to have_http_status(:found)
     end
@@ -90,7 +83,6 @@ RSpec.describe Lottery, type: :request do
   describe 'PATCH /lotteries/:id' do
     subject(:request_update) { patch lottery_path(lottery), params: }
 
-    let(:user) { create(:user) }
     let(:lottery) { create(:lottery) }
     let(:params) do
       {
@@ -106,7 +98,6 @@ RSpec.describe Lottery, type: :request do
     end
 
     it 'update lottery successful' do
-      sign_in user
       expect do
         request_update
         lottery.reload
@@ -118,12 +109,9 @@ RSpec.describe Lottery, type: :request do
   describe 'DELETE /lotteries/:id' do
     subject(:request_delete) { delete lottery_path(lottery) }
 
-    let(:user) { create(:user) }
     let!(:lottery) { create(:lottery) }
 
     it 'delete lottery successful' do
-      sign_in user
-
       expect { request_delete }.to change(described_class, :count).by(-1)
       expect(response).to have_http_status(:found)
     end
