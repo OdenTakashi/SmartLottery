eeeb0c82cbbb260170cb34fc787130640339551c
naoki.kodama <odentakashidesu2@gmail.com>
2024-08-18 21:01:38 +0900

    newページへのリクエストスペックを追加

--- spec/requests/lottery_spec.rb
+++ spec/requests/lottery_spec.rb
@@ -43,4 +43,59 @@ RSpec.describe Lottery, type: :request do
       expect(response).to have_http_status(:ok)
     end
   end
+
+  describe 'GET /lotteries/new' do
+    subject(:request_new) { get new_lottery_path }
+
+    let(:user) { create(:user) }
+
+    it 'return 200 status' do
+      sign_in user
+      request_new
+
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  # describe 'POST /lotteries' do
+  #   subject(:request_show) { get lottery_path(lottery) }
+
+  #   let(:user) { create(:user) }
+  #   let(:lottery) { create(:lottery) }
+
+  #   it 'return 200 status' do
+  #     sign_in user
+  #     request_show
+
+  #     expect(response).to have_http_status(:ok)
+  #   end
+  # end
+
+  # describe 'PATCH /lotteries/:id' do
+  #   subject(:request_show) { get lottery_path(lottery) }
+
+  #   let(:user) { create(:user) }
+  #   let(:lottery) { create(:lottery) }
+
+  #   it 'return 200 status' do
+  #     sign_in user
+  #     request_show
+
+  #     expect(response).to have_http_status(:ok)
+  #   end
+  # end
+
+  # describe 'DELETE /lotteries/:id' do
+  #   subject(:request_show) { get lottery_path(lottery) }
+
+  #   let(:user) { create(:user) }
+  #   let(:lottery) { create(:lottery) }
+
+  #   it 'return 200 status' do
+  #     sign_in user
+  #     request_show
+
+  #     expect(response).to have_http_status(:ok)
+  #   end
+  # end
 end
